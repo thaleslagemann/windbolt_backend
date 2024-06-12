138b7ff11fd79eb1a0078c95ce683c48ce796269
Thales Lagemann <thalesalaugustol@gmail.com>
2024-06-11 21:07:45 -0300

    add insert user route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('./db');
 
 router.get('/', (req, res) => {
     res.send('this is user route');
@@ -10,8 +11,20 @@ router.get('/:userId', (req, res) => {
     res.send(`Este é o usuário com o ID: ${userId}`);
 });
 
-router.post('/', (req, res) => {
-    res.send('Usuário criado!');
+router.post('/users/insert', async (req, res) => {
+    const { tag, name, uuid } = req.body;
+
+    if (!tag || !name || !uuid) {
+        return res.status(400).send('Dados incompletos');
+    }
+
+    try {
+        const result = pool.query('INSERT INTO users (tag, name, uuid) VALUES ($1, $2, $3) RETURNING *');
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        console.error('Erro ao inserir usuário', err);
+        res.status(500).send('Erro ao inserir usuário no banco de dados');
+    }
 });
 
 module.exports = router;
